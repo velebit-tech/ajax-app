39363395df092001116118b63c385ba46eb65a7f
Mario Markovic <velebit.business@gmail.com>
2020-04-08 09:32:49 +0200

    section 9: first commit - working with axios

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+    componentDidMount () {
+
+    }
+
+    render () {
+        return (
+            <div>
+                <section className="Posts">
+                    <Post />
+                    <Post />
+                    <Post />
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
